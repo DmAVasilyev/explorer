28f627c0ca4ee605c7a0bcab3e4f58b618782a8f
DmAVasilyev <apmdimon@gmail.com>
2018-08-28 22:56:07 +0300

    Проект создан

---
+++ src/Explorer.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class Explorer {
+            public static void main (String[] args) {
+            java.io.File file = new java.io.File("C:\\Program Files");
+            String list[] = file.list();
+            for (String s : list) {
+                System.out.println(s);
+            }
+        }
+    }
+
+
+
