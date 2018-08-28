f7cdff35e97ea3f11deb2cea260caa02fa605752
DmAVasilyev <apmdimon@gmail.com>
2018-08-28 23:36:38 +0300

    Создан конструктор класс UI

--- src/UI.java
+++ src/UI.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
 
-public class UI {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class UI extends JFrame {
+    private JPanel catalogPanel = new JPanel();
+    private JList filesList = new JList();
+    private JScrollPane filesScroll = new JScrollPane(filesList);
+    private JPanel buttonsPanel = new JPanel();
+    private JButton addButton = new JButton("Создать папку");
+    private JButton backButton = new JButton("Назад");
+    private JButton deleteButton = new JButton("Удалить");
+    private JButton renameButton = new JButton("Переименовать");
+    private ArrayList <String> dirscache = new ArrayList<>();
+
+    public UI () {
+        super("Проводник");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(true);
+        catalogPanel.setLayout(new BorderLayout(5, 5));
+        catalogPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        buttonsPanel.setLayout(new GridLayout(1,4,5,5));
+
+    }
+
 }
